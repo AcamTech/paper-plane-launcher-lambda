45eb6216aedf0ecba156903898d2520c2e66ef65
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-02-22 08:58:34 +0100

    Added request types

--- src/index.js
+++ src/index.js
@@ -1,5 +1,20 @@
+const REQUEST_TYPES = Object.freeze({
+  UNKNOWN: 'Unknown',
+  TURN_ON: 'TurnOn',
+  TURN_OFF: 'TurnOff',
+  DISCOVER: 'Discover',
+  REPORT_STATE: 'ReportState'
+})
+exports.REQUEST_TYPES = REQUEST_TYPES
+
+exports.getRequestType = event => {
+  try {
+    return event.directive.header.name
+  } catch (error) {
+    return REQUEST_TYPES.UNKNOWN
+  }
+}
+
 exports.handler = (event, context) => {
 
 }
-
-exports.getRequestType = event => event.directive.header.name
--- tests/index.test.js
+++ tests/index.test.js
@@ -5,12 +5,25 @@ const events = require('./events.json')
 const responses = require('./responses.json')
 
 const {
-  getRequestType
+  getRequestType,
+  REQUEST_TYPES
 } = lambda
+
+const {
+  UNKNOWN,
+  TURN_ON
+} = REQUEST_TYPES
 
 test('finds the alexa request type', () => {
   const event = events.turnOn
-  const expected = event.directive.header.name
+  const expected = TURN_ON
+  const actual = getRequestType(event)
+  expect(actual).toBe(expected)
+})
+
+test('returns unknown request type', () => {
+  const event = {}
+  const expected = UNKNOWN
   const actual = getRequestType(event)
   expect(actual).toBe(expected)
 })
