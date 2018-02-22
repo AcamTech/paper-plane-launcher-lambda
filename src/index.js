021ace55b7327987a9d8abaaa085589a4090ddbc
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-02-22 20:58:07 +0100

    Added handler for discovery requests

--- src/index.js
+++ src/index.js
@@ -5,9 +5,41 @@ const REQUEST_TYPES = Object.freeze({
   DISCOVER: 'Discover',
   REPORT_STATE: 'ReportState'
 })
-exports.REQUEST_TYPES = REQUEST_TYPES
 
-exports.getRequestType = event => {
+const RESPONSE_TYPES = Object.freeze({
+  DISCOVER: "Discover.Response",
+  TURN_ON: "ON",
+  TURN_OFF: "OFF"
+})
+
+const CAPABILITIES = Object.freeze([
+  {
+    "type": "AlexaInterface",
+    "interface": "Alexa",
+    "version": "3"
+  },
+  {
+    "interface": "Alexa.PowerController",
+    "version": "3",
+    "type": "AlexaInterface",
+    "properties": {
+      "supported": [{
+        "name": "powerState"
+      }],
+       "retrievable": true
+    }
+  }
+])
+
+const BASE_ENDPOINT = Object.freeze({
+  "manufacturerName": "Arduino",
+  "friendlyName": "Paper Plane Launcher",
+  "description": "Launcher of planes made of paper",
+  "displayCategories": ["SWITCH"],
+  capabilities: CAPABILITIES
+})
+
+const getRequestType = event => {
   try {
     return event.directive.header.name
   } catch (error) {
@@ -15,6 +47,23 @@ exports.getRequestType = event => {
   }
 }
 
-exports.handler = (event, context) => {
-
+// Expected response https://developer.amazon.com/docs/device-apis/alexa-discovery.html#discoverresponse
+const handleAlexaDiscoverRequest = event => {
+  // Use the messageId as an identifier of the device/thing/launcher
+  const endpointId = event.directive.header.messageId
+  const endpoint = Object.assign({ endpointId }, BASE_ENDPOINT)
+  const endpoints = [endpoint]
+  const payload = { endpoints }
+  const header = Object.assign({}, event.directive.header, { name: RESPONSE_TYPES.DISCOVER })
+  return { event: { header, payload } }
 }
+
+const handler = (event, context) => {
+  
+}
+
+
+exports.REQUEST_TYPES = REQUEST_TYPES
+exports.getRequestType = getRequestType
+exports.handleAlexaDiscoverRequest = handleAlexaDiscoverRequest
+exports.handler = handler
--- tests/index.test.js
+++ tests/index.test.js
@@ -5,8 +5,9 @@ const events = require('./events.json')
 const responses = require('./responses.json')
 
 const {
+  REQUEST_TYPES,
   getRequestType,
-  REQUEST_TYPES
+  handleAlexaDiscoverRequest
 } = lambda
 
 const {
@@ -14,16 +15,51 @@ const {
   TURN_ON
 } = REQUEST_TYPES
 
-test('finds the alexa request type', () => {
-  const event = events.turnOn
-  const expected = TURN_ON
-  const actual = getRequestType(event)
-  expect(actual).toBe(expected)
+describe('getRequestType', () => {
+  test('finds the alexa request type', () => {
+    const event = events.turnOn
+    const expected = TURN_ON
+    const actual = getRequestType(event)
+    expect(actual).toBe(expected)
+  })
+
+  test('returns unknown request type', () => {
+    const event = {}
+    const expected = UNKNOWN
+    const actual = getRequestType(event)
+    expect(actual).toBe(expected)
+  })
 })
 
-test('returns unknown request type', () => {
-  const event = {}
-  const expected = UNKNOWN
-  const actual = getRequestType(event)
-  expect(actual).toBe(expected)
+describe('handleAlexaDiscoverRequest', () => {
+  const event = events.discover
+  const response = handleAlexaDiscoverRequest(event)
+
+  test('event.header.name to be "Discover.Response"', () => {
+    const expected = 'Discover.Response'
+    const actual = response.event.header.name
+    expect(actual).toBe(expected)
+  })
+
+  describe('endpoint', () => {
+    const endpoint = response.event.payload.endpoints[0]
+    test('endpointId to be messageId', () => {
+      const expected = event.directive.header.messageId
+      const actual = endpoint.endpointId
+      expect(actual).toBe(expected)
+    })
+
+    test('displayCategories contains "SWITCH"', () => {
+      const actual = endpoint.displayCategories
+      const expected = ['SWITCH']
+      expect(actual).toEqual(expected)
+    })
+
+    test('capabilities contains interface "Alexa" and "Alexa.PowerController"', () => {
+      const { capabilities } = endpoint
+      const actual = capabilities.map(({ interface }) => interface).sort()
+      const expected = ['Alexa', 'Alexa.PowerController'].sort()
+      expect(actual).toEqual(expected)
+    })
+  })
 })
