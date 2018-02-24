9e7d8d4195873b0e981b3aee7e03d82359dbd1a1
Jonathan Morales Vélez <jonathan@themidgame.com>
2018-02-24 12:53:02 +0100

    Added comments explaining the lambda workflow

--- src/index.js
+++ src/index.js
@@ -4,9 +4,12 @@ const broadcaster = require('./broadcaster')
 const handler = (event, context) => {
   const response = responseBuilder.buildResponseToEvent(event)
   if (!responseBuilder.isPowerControlRequest(event)) {
+    // Reply to Alexa if we don't need to communicate with the Arduino
     return context.succeed(response)
   }
 
+  // Send a message to the Arduino indicating what the user wants to do
+  // and then tell Alexa everything is ok.
   const value = responseBuilder.getPowerValue(event)
   broadcaster.send(value)
     .then(() => context.succeed(response))
